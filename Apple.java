219fc0378dc20346e335fae6da66e2d3b0ad3f97
Gao040701 <349968818@gapps.yrdsb.ca>
2023-05-04 09:04:34 -0400

    Add Elephant class and Apple class

--- Apple.java
+++ Apple.java
@@ -1,24 +1,20 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Elephant here.
+ * Write a description of class Apple here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Elephant extends Actor
+public class Apple extends Actor
 {
     /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
         // Add your action code here.
-        if (Greenfoot.isKeyDown("d")){
-            move(5);
-        }else if(Greenfoot.isKeyDown("a")){
-            move(-5);
-        }
+        setLocation(getX(), getY() + 1);
     }
 }
