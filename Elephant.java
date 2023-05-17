5d1ce403c864910662f65dc49795adfb42369f05
Gao040701 <349968818@gapps.yrdsb.ca>
2023-05-17 09:05:36 -0400

    Resize background and animation
    Resize background
    add basic animation for the elephant to change between different images

--- Elephant.java
+++ Elephant.java
@@ -9,10 +9,29 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
-    /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
+    GreenfootImage[] images = new GreenfootImage[8];
+
+    SimpleTimer animationTimer = new SimpleTimer();
+    public Elephant(){
+        for (int i = 0; i < images.length; i++){
+            images[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            images[i].scale(100,100);
+        }
+        setImage(images[0]);
+        animationTimer.mark();
+    }
+    
+    private int imageIndex = 0;
+    public void animateElephant(){
+        if (animationTimer.millisElapsed() < 300){
+            return;
+        }
+        animationTimer.mark(); 
+        
+        imageIndex = (imageIndex + 1) % images.length;
+        setImage(images[imageIndex]);
+    }
     public void act()
     {
         // Add your action code here.
@@ -23,6 +42,8 @@ public class Elephant extends Actor
         }
         
         eat();
+        
+        animateElephant();
     }
     
     public void eat(){
